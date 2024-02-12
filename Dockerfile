80bb7287fd82357d43ef923af31babd109de353f
Chuck Findlay <chuck@findlayis.me>
2024-02-11 20:20:18 -0400

    gcp image stub - needs testing

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 # Author: Chuck Findlay <chuck@findlayis.me>
 # License: LGPL v3.0
-FROM alpine:3
+FROM debian:bullseye-slim
 
 LABEL version="1.20.4"
 LABEL maintainer="chuck@findlayis.me"
@@ -8,7 +8,8 @@ LABEL maintainer="chuck@findlayis.me"
 ADD https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar /minecraft.jar
 
 RUN \
-    apk add openjdk17
+    apt-get update && \
+    apt-get install -y openjdk-17-jre-headless
 
 COPY start.sh /
 
---
+++ gcp-start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Start script handler for GCP minecraft-docker image
+#
+# Author: Chuck Findlay <chuck@findlayis.me>
+# License: LGPL v3.0
+
+echo "0 */1 * * * gcloud auth activate-service-account --key-file=/gcs-key.json && gsutil -m cp -r /root/Zomboid gs://$GCS_BUCKET$GCS_BUCKET_PATH/" > /etc/cron.d/gcs-hourly-backup
+chmod 0644 /etc/cron.d/gcs-hourly-backup
+crontab /etc/cron.d/gcs-hourly-backup
+
+# Start cron so it runs auto backups
+cron
+
+# Authenticate to GCS
+gcloud auth activate-service-account --key-file=/gcs-key.json
+
+# Copy files from GCS to /root (so /root/Zomboid is there)
+gsutil -m cp -r gs://$GCS_BUCKET$GCS_BUCKET_PATH/config /config/
+
+chmod +x /start.sh
+/start.sh
+
+# Copy files again once the server is done
+gsutil -m cp -r /config gs://$GCS_BUCKET$GCS_BUCKET_PATH/
